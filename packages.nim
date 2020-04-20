340eb258574f7a42aa39656bcf3f8a34791dded4
Leonardo Mariscal <leo@ldmd.mx>
2020-04-19 19:55:36 -0500

    Add mercurial support with hg-git

--- packages.nim
+++ packages.nim
@@ -1,11 +1,9 @@
 import httpclient, strutils, os, json
 
 proc sshToHttp(ssh: string): string =
-  let info = ssh.split(":")[1]
-  let parts = info.split("/")
-  let user = parts[0]
-  var repo = parts[1]
-  result = "https://github.com/" & user & "/" & repo
+  result = ssh
+  result = result.replace(":", "/")
+  result = result.replace("git@", "https://")
 
 putEnv("GIT_SSH_COMMAND", "ssh -oBatchMode=yes")
 putEnv("GIT_TERMINAL_PROMPT", "0")
@@ -14,8 +12,11 @@ let client = newHttpClient()
 client.downloadFile("https://raw.githubusercontent.com/nim-lang/packages/master/packages.json", "packages.json")
 let current = readFile("packages.json")
 var nodes = current.parseJson()
+var count = 0
+var fail = newSeq[string]()
 
 for obj in nodes.items:
+  count.inc
   if obj.contains("method"):
     if obj["method"].getStr("") == "git":
       let name = obj["name"].getStr()
@@ -23,13 +24,39 @@ for obj in nodes.items:
       if url.startsWith("git@"):
         url = url.sshToHttp()
       if not dirExists("../repos/" & name):
-        echo "Downloading " & obj["name"].getStr()
+        echo "Downloading " & obj["name"].getStr() & "... #" & $count
         let ret = execShellCmd("cd ../repos && git clone --bare " & url & " ./" & name)
         if ret != 0:
           echo "Not found " & name
+    elif obj["method"].getStr() == "hg":
+      echo "Downloading " & obj["name"].getStr() & "... hg #" & $count
+      let name = obj["name"].getStr()
+      var url = obj["url"].getStr()
+
+      if not dirExists("../hg/" & name):
+        let ret = execShellCmd("cd ../hg && hg clone " & url & " ./" & name)
+        if ret != 0:
+          fail.add(name)
+          echo "Not found " & name
+          continue
+      else:
+        let ret = execShellCmd("cd ../hg/" & name & " && hg pull")
+        if ret != 0:
+          fail.add(name)
+          echo "Not found " & name
+
+      if dirExists("../repos/" & name):
+        removeDir("../repos/" & name)
+      discard execShellCmd("cd ../repos && git init --bare ./" & name)
+      let ret = execShellCmd("cd ../hg/" & name & " && hg bookmarks hg && hg push ../../repos/" & name)
+      if ret != 0:
+        fail.add(name)
+        echo "Failed"
+      else:
+        echo "Done"
 
 for item in nodes.mitems:
-  if not item.contains("url"):
+  if not item.contains("url") or fail.contains(item["name"].getStr()):
     continue
   let name = item["name"].getStr()
   let parts = item["url"].getStr().split("?")
@@ -37,5 +64,6 @@ for item in nodes.mitems:
   if parts.len > 1:
     url.add("?" & parts[1])
   item["url"] = %* url
+  item["method"] = %* "git"
 
 writeFile("../reg/packages.json", nodes.pretty())
--- update.nim
+++ update.nim
@@ -1,11 +1,9 @@
 import json, os, strutils
 
 proc sshToHttp(ssh: string): string =
-  let info = ssh.split(":")[1]
-  let parts = info.split("/")
-  let user = parts[0]
-  var repo = parts[1]
-  result = "https://github.com/" & user & "/" & repo
+  result = ssh
+  result = result.replace(":", "/")
+  result = result.replace("git@", "https://")
  
 let packages = readFile("packages.json")
 let nodes = packages.parseJson()
@@ -18,7 +16,7 @@ putEnv("GIT_TERMINAL_PROMPT", "0")
 for obj in nodes.items:
   count.inc
   if obj.contains("method"):
-    if obj["method"].getStr("") == "git":
+    if obj["method"].getStr() == "git":
       echo "Downloading " & obj["name"].getStr() & "... #" & $count
       let name = obj["name"].getStr()
       var url = obj["url"].getStr().split("?")[0]
@@ -32,6 +30,31 @@ for obj in nodes.items:
       else:
         discard execShellCmd("cd ../repos/" & name & " && git fetch origin +refs/heads/*:refs/heads/* --prune")
       echo "Done"
+    elif obj["method"].getStr() == "hg":
+      echo "Downloading " & obj["name"].getStr() & "... hg #" & $count
+      let name = obj["name"].getStr()
+      var url = obj["url"].getStr()
+
+      if not dirExists("../hg/" & name):
+        let ret = execShellCmd("cd ../hg && hg clone " & url & " ./" & name)
+        if ret != 0:
+          notFound.add(name)
+          echo "Not found " & name
+          continue
+      else:
+        let ret = execShellCmd("cd ../hg/" & name & " && hg pull")
+        if ret != 0:
+          notFound.add(name)
+          echo "Not found " & name
+
+      if dirExists("../repos/" & name):
+        removeDir("../repos/" & name)
+      discard execShellCmd("cd ../repos && git init --bare ./" & name)
+      let ret = execShellCmd("cd ../hg/" & name & " && hg bookmarks hg && hg push ../../repos/" & name)
+      if ret != 0:
+        echo "Failed"
+      else:
+        echo "Done"
  
 var notFoundText = ""
 for f in notFound.items:
